4b1c6de237416971727884ccf7d8138581e210e9
unknown <jiteshs855@gmail.com>
2024-08-19 16:49:17 +0530

    Add find_client_ip function

--- Assignment1/throughput_plot.py
+++ Assignment1/throughput_plot.py
@@ -3,14 +3,12 @@
 import argparse
 import warnings
 
-import matplotlib.pyplot as plt
 from scapy.all import *
 from scapy.layers.inet import IP, TCP
 
-warnings.filterwarnings("ignore", category=ImportWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-ip1 = "61.246.223.11"
-ip2 = "192.168.29.159"
+server_ip = "61.246.223.11"
 tcp_port = 443
 
 filtered_packets = []
@@ -22,6 +20,49 @@ downlink_end_time = None
 total_uplink_bytes = 0
 uplink_start_time = None
 uplink_end_time = None
+client_ip = None
+uplink_speed_mbps = None
+downlink_speed_mbps = None
+
+
+def find_client_ip(pcap_file, server_ip):
+    global client_ip
+    packets = rdpcap(pcap_file)
+    ip_counts = defaultdict(int)
+
+    for pkt in packets:
+        if IP in pkt and TCP in pkt:
+            ip_header = pkt[IP]
+
+            if ip_header.src == server_ip:
+                ip_counts[ip_header.dst] += 1
+
+            elif ip_header.dst == server_ip:
+                ip_counts[ip_header.src] += 1
+
+    if ip_counts:
+        client_ip = max(ip_counts, key=ip_counts.get)
+        # print(f"Client IP is: {client_ip}")
+
+
+def filter(pcap_file):
+    packets = rdpcap(pcap_file)
+    # print(packets)  # gives info on number of TCP or UDP or ICMP packets
+    for pkt in packets:
+        if IP in pkt and TCP in pkt:
+            ip_header = pkt[IP]
+            tcp_header = pkt[TCP]
+            if (
+                (ip_header.src == server_ip and ip_header.dst == client_ip)
+                or (ip_header.src == client_ip and ip_header.dst == server_ip)
+            ) and (tcp_header.sport == tcp_port or tcp_header.dport == tcp_port):
+                filtered_packets.append(pkt)
+
+    # print(f"Number of filtered packets: {len(filtered_packets)}")
+    # print(f"Packet number 100 {filtered_packets[100]}")
+    # print(len(packets))  # gives number of packets
+    # pkt = packets[100]  # to examine the 100th packet
+    # print(pkt)
 
 
 def throughput():
@@ -29,98 +70,59 @@ def throughput():
     global total_uplink_bytes
     global downlink_start_time, downlink_end_time
     global uplink_start_time, uplink_end_time
-
-    downlink_throughput = {}
-    uplink_throughput = {}
+    global uplink_speed_mbps, downlink_speed_mbps
 
     for pkt in filtered_packets:
-        ip_layer = pkt[IP]
-        pkt_time = int(pkt.time)  # Round to the nearest second
-
-        if ip_layer.src == ip1 and ip_layer.dst == ip2:
+        ip_header = pkt[IP]
+        # print(pkt.time)
+        pkt_time = float(
+            pkt.time
+        )  # some unhashable type error, need it to be int or float
+        if ip_header.src == server_ip and ip_header.dst == client_ip:
             downlink.append(pkt)
+            # print(len(pkt))
             total_downlink_bytes += len(pkt)
-            downlink_throughput[pkt_time] = downlink_throughput.get(pkt_time, 0) + len(
-                pkt
-            )
             if downlink_start_time is None:
-                downlink_start_time = pkt.time  # Timestamp of the first packet
-            downlink_end_time = (
-                pkt.time
-            )  # Continuously update with the latest timestamp
-        elif ip_layer.src == ip2 and ip_layer.dst == ip1:
+                downlink_start_time = pkt_time
+            downlink_end_time = pkt_time
+        elif ip_header.src == client_ip and ip_header.dst == server_ip:
             uplink.append(pkt)
             total_uplink_bytes += len(pkt)
-            uplink_throughput[pkt_time] = uplink_throughput.get(pkt_time, 0) + len(pkt)
+
             if uplink_start_time is None:
-                uplink_start_time = pkt.time  # Timestamp of the first packet
-            uplink_end_time = pkt.time  # Continuously update with the latest timestamp
+                uplink_start_time = pkt_time
+            uplink_end_time = pkt_time
 
     if downlink_start_time and downlink_end_time:
-        time_interval = downlink_end_time - downlink_start_time  # Time in seconds
-        if time_interval > 0:
-            download_speed_bps = (
-                total_downlink_bytes * 8
-            ) / time_interval  # Speed in bits per second
-            download_speed_mbps = download_speed_bps / (1024 * 1024)  # Convert to Mbps
-            print(50 * "*")
-            print(f"Download speed in Mbps is {download_speed_mbps}")
-            print(50 * "*")
+        downlink_time_interval = downlink_end_time - downlink_start_time
+
+        downlink_speed_bps = (total_downlink_bytes * 8) / downlink_time_interval
+        downlink_speed_mbps = downlink_speed_bps / (2**20)
+        # print(downlink_time_interval)
+        # print(50 * "*")
+        # print(total_downlink_bytes)
+        # print(50 * "*")
+        # print(50 * "*")
 
     if uplink_start_time and uplink_end_time:
-        time_interval = uplink_end_time - uplink_start_time  # Time in seconds
-        if time_interval > 0:
-            upload_speed_bps = (
-                total_uplink_bytes * 8
-            ) / time_interval  # Speed in bits per second
-            upload_speed_mbps = upload_speed_bps / (1024 * 1024)  # Convert to Mbps
-            print(50 * "*")
-            print(f"Upload speed in Mbps is {upload_speed_mbps}")
-            print(50 * "*")
+        uplink_time_interval = uplink_end_time - uplink_start_time
 
-    return downlink_throughput, uplink_throughput
+        # print(uplink_time_interval)
+        uplink_speed_bps = (total_uplink_bytes * 8) / uplink_time_interval
+        uplink_speed_mbps = uplink_speed_bps / (2**20)
+        # print(total_uplink_bytes)
+        # print(50 * "*")
+        # print(50 * "*")
 
-
-def plot_throughput(downlink_throughput, uplink_throughput):
-    times = sorted(set(downlink_throughput.keys()).union(set(uplink_throughput.keys())))
-
-    downlink_speeds = [
-        downlink_throughput.get(t, 0) * 8 / (1024 * 1024) for t in times
-    ]  # Convert to Mbps
-    uplink_speeds = [
-        uplink_throughput.get(t, 0) * 8 / (1024 * 1024) for t in times
-    ]  # Convert to Mbps
-
-    plt.figure(figsize=(10, 6))
-
-    plt.plot(times, downlink_speeds, label="Download Throughput (Mbps)", color="blue")
-    plt.plot(times, uplink_speeds, label="Upload Throughput (Mbps)", color="green")
-
-    plt.xlabel("Time (seconds)")
-    plt.ylabel("Throughput (Mbps)")
-    plt.title("Throughput Over Time")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    print(f"{downlink_speed_mbps}, {uplink_speed_mbps}")
 
 
-def filter(pcap_file):
-    packets = rdpcap(pcap_file)
-    for pkt in packets:
-        if IP in pkt and TCP in pkt:
-            ip_layer = pkt[IP]
-            tcp_layer = pkt[TCP]
-            if (
-                (ip_layer.src == ip1 and ip_layer.dst == ip2)
-                or (ip_layer.src == ip2 and ip_layer.dst == ip1)
-            ) and (tcp_layer.sport == tcp_port or tcp_layer.dport == tcp_port):
-                filtered_packets.append(pkt)
-
-    print(f"Number of filtered packets: {len(filtered_packets)}")
+def plot():
+    print("yet to implement")
 
 
 def main():
-    parser = argparse.ArgumentParser(description="PCAP file to process")
+    parser = argparse.ArgumentParser(description="To perform speedtest")
     parser.add_argument("pcap_file", type=str, help="The PCAP file to process")
 
     group = parser.add_mutually_exclusive_group(required=True)
@@ -132,15 +134,15 @@ def main():
     )
 
     args = parser.parse_args()
+    find_client_ip(args.pcap_file, server_ip)
     filter(args.pcap_file)
 
     if args.plot:
-        print("In mode plot")
-        downlink_throughput, uplink_throughput = throughput()
-        plot_throughput(downlink_throughput, uplink_throughput)
+        # print("In mode plot")
+        plot()
 
     elif args.throughput:
-        print("In mode throughput")
+        # print("In mode throughput")
         throughput()
 
 
